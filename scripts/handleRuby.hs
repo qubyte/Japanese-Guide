e7e33e500c149bb8f8f12089ce53944dee2f43d7
Mark S. Everitt <mark.s.everitt@gmail.com>
2012-01-23 16:11:48 +0900

    Added Pandoc filter to handle link-like ruby syntax.

---
+++ scripts/handleRuby.hs
@@ -0,0 +1,16 @@
+﻿-- handleRuby.hs
+import Text.Pandoc
+
+handleRuby :: Inline -> Inline
+handleRuby (Link (Str ruby : []) ('-':kanji,_)) = RawInline "html" rubyHtml
+  where rubyHtml = ("<ruby>" ++ kanji ++ "<rp>(</rp><rt>" ++ ruby ++ "</rt><rp>)</rp></ruby>")
+handleRuby x = x
+
+readDoc :: String -> Pandoc
+readDoc = readMarkdown defaultParserState
+
+writeDoc :: Pandoc -> String
+writeDoc = writeMarkdown defaultWriterOptions
+
+main :: IO ()
+main = interact (writeDoc . bottomUp handleRuby . readDoc)
